47d596f6045daa75d8f9cd78ce39cdb367b63fe3
Tom Hendrikx <tom@whyscream.net>
2026-07-03 02:35:05 +0200

    Refactor the version detection, no more workarounds needed

--- src/dspam/__init__.py
+++ src/dspam/__init__.py
@@ -1,10 +1,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
-import importlib.metadata
 
-__version__ = importlib.metadata.version("python-dspam")
+"""Some constants used throughout the application"""
 
-# Some constants used throughout the application
+# TODO: change these into an Enum class or whatnot
 IS_INNOCENT: str = "innocent"
 IS_SPAM: str = "spam"
 IS_UNKNOWN: str = "unknown"
